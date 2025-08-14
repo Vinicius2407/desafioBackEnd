56c17518c66742869b7492ec18900f2374fd9725
Vinicius Silva <viniciussilvapereira24@gmail.com>
2025-08-14 17:10:34 -0300

    feat: metodo para padronizacao de erro

---
+++ WebApp/Controllers/ApiController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers;
+
+public class ApiController : ControllerBase
+{
+
+
+    protected IActionResult Error(int code, string message)
+    {
+        var errorResponse = new ResponseError(code, message);
+        return BadRequest(errorResponse);
+    }
+}
+
+public class ResponseError
+{
+    public ResponseError(int code, string message)
+    {
+        StatusCode = code;
+        Error = message;
+    }
+    public string Error { get; set; } = string.Empty;
+    public int StatusCode { get; set; }
+}
