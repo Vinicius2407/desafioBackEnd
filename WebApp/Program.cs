0951e61897ad6250984b51f888943558d3bc7dc7
Vinicius Silva <viniciussilvapereira24@gmail.com>
2025-08-15 12:59:52 -0300

    feat: trocando metodo de mostrar enum no swagger

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -47,7 +47,11 @@ builder.Services.AddAuthentication("Bearer")
     });
 
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+                .AddJsonOptions(options =>
+                                {
+                                    options.JsonSerializerOptions.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter());
+                                });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
@@ -62,7 +66,6 @@ builder.Services.AddSwaggerGen(c =>
         Scheme = "bearer",
         BearerFormat = "JWT"
     });
-    c.SchemaFilter<EnumSchemaFilter>();
     c.OperationFilter<AddSecurityRequirement>();
 });
 
